6cf2eb434dce670bf80cef8dc7cb7a54d160d731
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-29 10:19:19 -0700

    Both are displaying properly when changes to the color are made. Updated the data to just be tile, should be easier/doable.

--- tile_flipper/src/components/tiles.js
+++ tile_flipper/src/components/tiles.js
@@ -5,17 +5,24 @@ import tilesData from "../dataForTiles"
 // import "../App.css"
 
 const Tiles = () => {
-  
   const [tiles, setTiles] = useState(tilesData)
+  console.log(tilesData)
 
   return (
     <div className="tilesWrapper">
       {tiles.map((tile, idx) => {
-        console.log("tile", tile, "id", idx)
-        
+        // console.log("tile", tile, "id", idx)
+        // console.log(tile.tile)
+        if (tile.tile === "orange") {
         return (
             <TileSuccess className="tile" key={idx} />
         )
+      }
+        if(tile.tile === "red") {
+          return (
+            <TileFailure className="tile" key={idx} />
+          )
+        }
       })}
     </div>
   )
